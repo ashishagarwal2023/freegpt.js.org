355cf6345bc9e70ed74e1c700ddba8f3e0f7a4a0
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-14 11:58:00 +0000

    Pending changes exported from your codespace

--- plan.js
+++
@@ -1,9 +0,0 @@
-/**
-* The way we get the response from the API
-* https://you.com/api/streamingSearch?q=query&page=page&count=10&domain=youchat
- [1] @param {string} query - Query, what to ask
- [2] @param {number} page  - Ideal to re-write responses however it does not always rewrite
- [3] the last domain parameter is needed to be youchat or it will reject it.
-
- * Just the plan here
- */
